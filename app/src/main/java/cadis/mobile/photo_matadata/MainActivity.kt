44611611ed9adb6adf443553aa568cf3636861a9
Ivelina Balcheva <ivab1414@gmail.com>
2022-11-07 17:07:02 +0200

    Добавяне на минимална визуализация. Акривиране на права на потребителя и визуализация на обекти пред камерата.

--- app/src/main/java/cadis/mobile/photo_matadata/MainActivity.kt
+++ app/src/main/java/cadis/mobile/photo_matadata/MainActivity.kt
@@ -2,10 +2,68 @@ package cadis.mobile.photo_matadata
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
+import cadis.mobile.photo_matadata.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
+import com.google.common.util.concurrent.ListenableFuture
+import java.util.concurrent.ExecutorService
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var cameraProviderFuture: ListenableFuture<ProcessCameraProvider>
+    private lateinit var cameraSelector: CameraSelector
+    private var imageCapture: ImageCapture? = null
+    private lateinit var imgCaptureExecutor: ExecutorService
+
+    private val cameraProviderResult = registerForActivityResult(ActivityResultContracts.RequestPermission()){ permissionGranted->
+        if(permissionGranted){
+            // cut and paste the previous startCamera() call here.
+            startCamera()
+        }else {
+            Snackbar.make(binding.root,"The camera permission is required", Snackbar.LENGTH_INDEFINITE).show()
+        }
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+        cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+        cameraProviderResult.launch(android.Manifest.permission.CAMERA)
+    }
+
+    companion object {
+        val TAG = "MainActivity"
+    }
+
+    private fun startCamera(){
+        // listening for data from the camera
+        cameraProviderFuture.addListener({
+            val cameraProvider = cameraProviderFuture.get()
+
+            // connecting a preview use case to the preview in the xml file.
+            val preview = Preview.Builder().build().also{
+                it.setSurfaceProvider(binding.preview.surfaceProvider)
+            }
+            try{
+                // clear all the previous use cases first.
+                cameraProvider.unbindAll()
+                // binding the lifecycle of the camera to the lifecycle of the application.
+                cameraProvider.bindToLifecycle(this,cameraSelector,preview)
+            } catch (e: Exception) {
+                Log.d(TAG, "Use case binding failed")
+            }
+
+        }, ContextCompat.getMainExecutor(this))
     }
 }
