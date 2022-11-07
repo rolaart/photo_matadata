fb09dcdb3b5a8d36256ae4f953273fac49dd476c
Ivelina Balcheva <ivab1414@gmail.com>
2022-11-07 18:23:26 +0200

    Стандартна фънкционалност на камерата. Добавена галерия за снимки. Фойловете съдържат 2ра страница за галерията и адаптер, който да подрежда снимките във изгледа на галерията.

---
+++ app/src/main/java/cadis/mobile/photo_matadata/GalleryAdapter.kt
@@ -0,0 +1,29 @@
+package cadis.mobile.photo_matadata
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import cadis.mobile.photo_matadata.databinding.ListItemImgBinding
+import com.bumptech.glide.Glide
+import java.io.File
+
+class GalleryAdapter(private val fileArray: Array<File>) :
+    RecyclerView.Adapter<GalleryAdapter.ViewHolder>() {
+    class ViewHolder(private val binding: ListItemImgBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(file: File) {
+            Glide.with(binding.root).load(file).into(binding.localImg)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        return ViewHolder(ListItemImgBinding.inflate(layoutInflater, parent, false))
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(fileArray[position])
+    }
+
+    override fun getItemCount() = fileArray.size
+}
--- app/src/main/java/cadis/mobile/photo_matadata/MainActivity.kt
+++ app/src/main/java/cadis/mobile/photo_matadata/MainActivity.kt
@@ -1,18 +1,28 @@
 package cadis.mobile.photo_matadata
 
+import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.content.ContextCompat
+import androidx.core.net.toUri
 import cadis.mobile.photo_matadata.databinding.ActivityMainBinding
 import com.google.android.material.snackbar.Snackbar
 import com.google.common.util.concurrent.ListenableFuture
+import java.io.File
 import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -20,6 +30,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var cameraSelector: CameraSelector
     private var imageCapture: ImageCapture? = null
     private lateinit var imgCaptureExecutor: ExecutorService
+
 
     private val cameraProviderResult = registerForActivityResult(ActivityResultContracts.RequestPermission()){ permissionGranted->
         if(permissionGranted){
@@ -40,6 +51,20 @@ class MainActivity : AppCompatActivity() {
         cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
 
         cameraProviderResult.launch(android.Manifest.permission.CAMERA)
+
+        imgCaptureExecutor = Executors.newSingleThreadExecutor()
+
+        binding.imgCaptureBtn.setOnClickListener{
+            takePhoto()
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                animateFlash()
+            }
+        }
+
+        binding.galleryBtn.setOnClickListener {
+            val intent = Intent(this, GalleryActivity::class.java)
+            startActivity(intent)
+        }
     }
 
     companion object {
@@ -55,15 +80,60 @@ class MainActivity : AppCompatActivity() {
             val preview = Preview.Builder().build().also{
                 it.setSurfaceProvider(binding.preview.surfaceProvider)
             }
+            imageCapture = ImageCapture.Builder().build()
+
             try{
                 // clear all the previous use cases first.
                 cameraProvider.unbindAll()
                 // binding the lifecycle of the camera to the lifecycle of the application.
-                cameraProvider.bindToLifecycle(this,cameraSelector,preview)
+                cameraProvider.bindToLifecycle(this,cameraSelector,preview,imageCapture)
             } catch (e: Exception) {
                 Log.d(TAG, "Use case binding failed")
             }
 
         }, ContextCompat.getMainExecutor(this))
+    }
+
+    private fun takePhoto(){
+        imageCapture?.let{
+            //Create a storage location whose fileName is timestamped in milliseconds.
+            val fileName = "JPEG_${System.currentTimeMillis()}"
+            val file = File(externalMediaDirs[0],fileName)
+
+            // Save the image in the above file
+            val outputFileOptions = ImageCapture.OutputFileOptions.Builder(file).build()
+
+            /* pass in the details of where and how the image is taken.(arguments 1 and 2 of takePicture)
+            pass in the details of what to do after an image is taken.(argument 3 of takePicture) */
+
+            it.takePicture(
+                outputFileOptions,
+                imgCaptureExecutor,
+                object : ImageCapture.OnImageSavedCallback {
+                    override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults){
+                        Log.i(TAG,"The image has been saved in ${file.toUri()}")
+                    }
+
+                    override fun onError(exception: ImageCaptureException) {
+                        Toast.makeText(
+                            binding.root.context,
+                            "Error taking photo",
+                            Toast.LENGTH_LONG
+                        ).show()
+                        Log.d(TAG, "Error taking photo:$exception")
+                    }
+
+                })
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    private fun animateFlash() {
+        binding.root.postDelayed({
+            binding.root.foreground = ColorDrawable(Color.WHITE)
+            binding.root.postDelayed({
+                binding.root.foreground = null
+            }, 50)
+        }, 100)
     }
 }
